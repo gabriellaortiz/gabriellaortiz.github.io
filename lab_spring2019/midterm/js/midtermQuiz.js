da9a3cba91035ed4c61e3162b4e0b6981e986259
gabriellaortiz <hellogabriellaortiz@gmail.com>
2019-03-24 22:25:52 -0400

    commit

--- lab_spring2019/midterm/js/midtermQuiz.js
+++ lab_spring2019/midterm/js/midtermQuiz.js
@@ -86,6 +86,49 @@ var questionContainer = document.getElementById('container');
 var beginInfo = document.getElementById('beginInfo');
 nextButton.addEventListener('click', nextButtonClicked);
 
+
+//trying to make enter key call nextButtonClicked function
+// if (i > 0) {
+// 	// Get the input field
+// 	var input = document.getElementById("useranswer");
+// 	// Execute a function when the user releases a key on the keyboard
+// 	input.addEventListener("keyup", function(event) {
+// 	  // Number 13 is the "Enter" key on the keyboard
+// 	  if (event.keyCode === 13) {
+// 	    // Cancel the default action, if needed
+// 	    event.preventDefault();
+// 	    // Trigger the button element with a click
+// 	    document.getElementById("nextButton").click();
+// 	  }
+// 	});
+// }
+
+
+
+
+// // Get the input field
+// var input = document.getElementById("useranswer");
+
+// // Execute a function when the user releases a key on the keyboard
+// input.addEventListener("keyup", function(event) {
+//   // Number 13 is the "Enter" key on the keyboard
+//   if (event.keyCode === 13) {
+//     // Cancel the default action, if needed
+//     event.preventDefault();
+//     // Trigger the button element with a click
+//     document.getElementById("nextButton").click();
+//   }
+// });
+
+// taskInput.addEventListener('keyup', function (event) {
+//   //check to see if the enter key was pressed
+//   if (event.which === 13) {
+//     //if so, run the addTask function
+//     nextButtonClicked();
+//   }
+// });
+
+
 //Initiating some variables
 var i = 0;
 var length1 = allQuestions.length;
